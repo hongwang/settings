d222c39450b44e4872a15cbb62a8f8d74d983a00
HongWang <xunhongwang@outlook.com>
2016-03-14 15:36:50 +0800

    refactor

--- bash/homemode.sh
+++ bash/homemode.sh
@@ -1,19 +1,18 @@
 if [[ "`uname`" == "Linux" ]]; then
-  echo "I'm linux"
+	echo "I'm linux"
 elif [[ "`uname`" == "Darwin" ]]; then
- PATH=$HOME'/n/bin':$PATH
- export PATH
+	PATH=$HOME'/n/bin':$PATH
 elif [[ "`uname`" == "Win32" ]]; then
- PATH='/c/Windows/Microsoft.NET/Framework/v4.0.30319':$PATH
- PATH='/d/Program Files/Sublime Text 3':$PATH
- export PATH
- export repos='/e/repos'
- export own=$repos'/own'
- export sts=$repos'/own/settings'
- export lnet=$repos'/dotNet'
- export ljs=$repos'/javascript'
- export lasp=$repos'/aspnet'
+	PATH='/c/Windows/Microsoft.NET/Framework/v4.0.30319':$PATH
+	PATH='/d/Program Files/Sublime Text 3':$PATH
+	export repos='/e/repos'
+	export own=$repos'/own'
+	export sts=$repos'/own/settings'
+	export lnet=$repos'/dotNet'
+	export ljs=$repos'/javascript'
+	export lasp=$repos'/aspnet'
 fi
+export PATH
 
 # Mysql
 alias mysql='mycli -h localhost -u root'
